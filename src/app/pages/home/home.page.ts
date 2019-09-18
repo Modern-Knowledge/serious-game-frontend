075c9fd9483a4953c7a986f2bc7974c6167eec93
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-09-18 23:56:34 +0200

    [TASK] get user data in home component

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/lib/models/User';
+import { AuthService } from 'src/app/providers/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  private user: User;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(){
+    this.authService.getRelatedUser().subscribe(user => {
+      this.user = user;
+    }); 
+  }
 
 }
