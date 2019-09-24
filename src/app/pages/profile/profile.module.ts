583a3133165cd2c5ff7a46b371f478a7259eda0d
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-09-24 20:46:14 +0200

    [BUGFIX] register components in profile module

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -6,6 +6,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { ProfilePage } from './profile.page';
+import { TherapistProfileComponent } from 'src/app/components/therapist/therapist-profile/therapist-profile.component';
+import { PatientProfileComponent } from 'src/app/components/patient/patient-profile/patient-profile.component';
+import { PatientSelectorComponent } from 'src/app/components/patient/patient-selector/patient-selector.component';
 
 const routes: Routes = [
   {
@@ -21,6 +24,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [ProfilePage]
+  declarations: [ProfilePage, TherapistProfileComponent, PatientProfileComponent, PatientSelectorComponent]
 })
 export class ProfilePageModule {}
