549b96158cd7bd279cdd0a907291bd060f470aa7
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-09-25 21:25:13 +0200

    [FEATURE] add main menu

---
+++ src/app/pages/main-menu/main-menu.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'serious-game-main-menu',
+  templateUrl: './main-menu.page.html',
+  styleUrls: ['./main-menu.page.scss'],
+})
+export class MainMenuPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
