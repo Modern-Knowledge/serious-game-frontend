d0af8e18fe6a5cadf2b06cd75e9f980a16a1bdf7
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-09-28 17:03:41 +0200

    [TASK] use dragula in day planning

--- src/app/pages/game/day-planning/day-planning.module.ts
+++ src/app/pages/game/day-planning/day-planning.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { DayPlanningPage } from './day-planning.page';
+import { DayPlanningPage } from "./day-planning.page";
+import { DragulaModule } from "ng2-dragula";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: DayPlanningPage
   }
 ];
@@ -19,7 +20,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    DragulaModule
   ],
   declarations: [DayPlanningPage]
 })
--- src/app/pages/game/day-planning/day-planning.page.ts
+++ src/app/pages/game/day-planning/day-planning.page.ts
@@ -5,6 +5,7 @@ import { Recipe } from "src/lib/models/Recipe";
 import { Word } from "src/lib/models/Word";
 import { WordService } from "src/app/providers/word.service";
 import { RecipeService } from "src/app/providers/recipe.service";
+import { DragulaService } from "ng2-dragula";
 
 @Component({
   selector: "serious-game-day-planning",
@@ -18,7 +19,8 @@ export class DayPlanningPage {
   constructor(
     private authService: AuthService,
     private wordService: WordService,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private dragulaService: DragulaService
   ) {}
 
   ionViewWillEnter() {
@@ -31,5 +33,19 @@ export class DayPlanningPage {
     this.recipeService.getAll().subscribe(recipes => {
       this.recipes = recipes;
     });
+  }
+
+  mixWordsWithRecipes() {
+    if (this.words && this.recipes) {
+      return [...this.words, ...this.recipes];
+    }
+  }
+
+  doReorder(event: any) {
+    event.detail.complete();
+  }
+
+  onSubmit() {
+    console.log("valid");
   }
 }
