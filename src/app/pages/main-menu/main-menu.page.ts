e7f77a9f4a95cd99af6220fc04e96231c5a86195
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-09-28 12:54:03 +0200

    [TASK] link to day planning page

--- src/app/pages/main-menu/main-menu.page.ts
+++ src/app/pages/main-menu/main-menu.page.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'serious-game-main-menu',
-  templateUrl: './main-menu.page.html',
-  styleUrls: ['./main-menu.page.scss'],
+  selector: "serious-game-main-menu",
+  templateUrl: "./main-menu.page.html",
+  styleUrls: ["./main-menu.page.scss"]
 })
 export class MainMenuPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
