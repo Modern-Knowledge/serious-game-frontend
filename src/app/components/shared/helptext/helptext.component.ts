f601ed238fdc7c58348a9efbf43750243becccce
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-10-03 22:20:29 +0200

    [TASK] add helptext component

---
+++ src/app/components/shared/helptext/helptext.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'serious-game-helptext',
+  templateUrl: './helptext.component.html',
+  styleUrls: ['./helptext.component.scss'],
+})
+export class HelptextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
