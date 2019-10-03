817d5e53b3cd3e42850b500262c986652c6d5689
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-10-03 20:43:08 +0200

    [TASK] deserialize patient data

--- src/app/providers/patient.service.ts
+++ src/app/providers/patient.service.ts
@@ -3,6 +3,7 @@ import { Observable } from "rxjs";
 import { Patient } from "src/lib/models/Patient";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
+import { HttpResponse } from 'src/lib/utils/http/HttpResponse';
 
 @Injectable({
   providedIn: "root"
@@ -11,15 +12,19 @@ export class PatientService {
   constructor(private http: HttpClient) {}
 
   get(id): Observable<Patient> {
-    return this.http.get<Patient>(`patients/${id}`);
+    return this.http.get<HttpResponse>(`patients/${id}`)
+    .pipe(
+      map(patient => new Patient().deserialize(new HttpResponse().deserialize(patient).data)));
   }
 
   getAll(): Observable<Patient[]> {
     return this.http
-      .get<Patient[]>(`patients`)
+      .get<HttpResponse>(`patients`)
       .pipe(
-        map(patients =>
-          patients.map(patient => new Patient().deserialize(patient))
+        map(patients => {
+          const patientsModel = new HttpResponse().deserialize(patients);
+          return patientsModel.data.map(patient => new Patient().deserialize(patient))
+        } 
         )
       );
   }
