cfc2ab759ae0c9b335ba319dddd09e4306b04152
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-10-05 08:56:03 +0200

    [FEATURE] display helptext

--- src/app/components/shared/helptext/helptext.component.ts
+++ src/app/components/shared/helptext/helptext.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Helptext } from "src/lib/models/Helptext";
 
 @Component({
-  selector: 'serious-game-helptext',
-  templateUrl: './helptext.component.html',
-  styleUrls: ['./helptext.component.scss'],
+  selector: "serious-game-helptext",
+  templateUrl: "./helptext.component.html",
+  styleUrls: ["./helptext.component.scss"]
 })
 export class HelptextComponent implements OnInit {
+  @Input() helptext: Helptext;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }
