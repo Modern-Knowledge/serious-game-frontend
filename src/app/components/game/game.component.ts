adc2e7c07d5154e5bd77c33ed5c6748fe3da7b91
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-10-10 19:52:38 +0200

    [TASK] use abstractmodel in gamecomponent interface

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -2,9 +2,10 @@ import { Recipe } from "src/lib/models/Recipe";
 import { Word } from "src/lib/models/Word";
 import { Game } from "src/lib/models/Game";
 import { EventEmitter } from "@angular/core";
+import { AbstractModel } from "src/lib/models/AbstractModel";
 
 export interface GameComponent {
-  data: (Recipe | Word)[];
+  data: AbstractModel<any>[];
   game: Game;
   event: EventEmitter<any>;
 }
--- src/app/components/game/shopping-center/shopping-center.component.ts
+++ src/app/components/game/shopping-center/shopping-center.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { Game } from "src/lib/models/Game";
 import { Ingredient } from "src/lib/models/Ingredient";
 import { IngredientService } from "src/app/providers/ingredient.service";
 import { Word } from "src/lib/models/Word";
+import { GameComponent } from "../game.component";
 
 @Component({
   selector: "serious-game-shopping-center",
   templateUrl: "./shopping-center.component.html",
   styleUrls: ["./shopping-center.component.scss"]
 })
-export class ShoppingCenterComponent implements OnInit {
+export class ShoppingCenterComponent implements OnInit, GameComponent {
+  @Input() data: Ingredient[];
   @Input() game: Game;
+  @Output() event: EventEmitter<any> = new EventEmitter();
 
   shoppingCart: Ingredient[];
   availableItems: (Ingredient | Word)[];
 
-  constructor(private ingredientService: IngredientService) {}
+  constructor() {}
 
   ngOnInit() {}
 }
