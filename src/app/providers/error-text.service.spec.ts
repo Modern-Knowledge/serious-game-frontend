970423fa9301be3f9426dce0131b66d5d311dab0
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-10-31 17:40:59 +0100

    [FEATURE] add service to get error texts from backend

---
+++ src/app/providers/error-text.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ErrorTextService } from './error-text.service';
+
+describe('ErrorTextService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: ErrorTextService = TestBed.get(ErrorTextService);
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/providers/error-text.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import { Errortext } from "src/lib/models/Errortext";
+import {
+  HttpResponse,
+  HttpResponseStatus
+} from "src/lib/utils/http/HttpResponse";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ErrorTextService {
+  constructor(private http: HttpClient) {}
+
+  get(id): Observable<Errortext> {
+    return this.http
+      .get<HttpResponse>(`errortexts/${id}`)
+      .pipe(
+        map(errortext =>
+          new Errortext().deserialize(
+            new HttpResponse().deserialize(errortext).data.errortext
+          )
+        )
+      );
+  }
+
+  getAll(): Observable<Errortext[]> {
+    return this.http.get<HttpResponse>(`errortexts`).pipe(
+      map(errorText => {
+        const errorTextsModel = new HttpResponse().deserialize(errorText);
+        return errorTextsModel.status === HttpResponseStatus.SUCCESS
+          ? errorTextsModel.data.errorText.map(Errortext =>
+              new Errortext().deserialize(Errortext)
+            )
+          : [];
+      })
+    );
+  }
+}
