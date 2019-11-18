90d76a3b06fc0f8ecd2c7283c6fb70b88140db98
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-11-18 22:24:05 +0100

    [FEATURE] parse errortexts

--- src/app/components/game/day-planning/mealtime/mealtime.component.ts
+++ src/app/components/game/day-planning/mealtime/mealtime.component.ts
@@ -1,37 +1,45 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { DragulaService } from 'ng2-dragula';
-import { Mealtimes } from 'src/lib/enums/Mealtimes';
-import { Errortext } from 'src/lib/models/Errortext';
-import { Recipe } from 'src/lib/models/Recipe';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { DragulaService } from "ng2-dragula";
+import { Mealtimes } from "src/lib/enums/Mealtimes";
+import { Errortext } from "src/lib/models/Errortext";
+import { Recipe } from "src/lib/models/Recipe";
+import { TemplateParser } from "src/lib/utils/TemplateParser";
 
 @Component({
-  selector: "serious-game-mealtime",
-  templateUrl: "./mealtime.component.html",
-  styleUrls: ["./mealtime.component.scss"]
+    selector: "serious-game-mealtime",
+    styleUrls: ["./mealtime.component.scss"],
+    templateUrl: "./mealtime.component.html"
 })
-export class MealtimeComponent implements OnInit {
-  @Input() title: string;
-  @Input() model: string;
-  @Input() mealtime: Mealtimes;
-  @Input() errorTexts: Errortext[];
-  @Output() event: EventEmitter<Recipe> = new EventEmitter<Recipe>();
-  @Output() errorEvent: EventEmitter<any> = new EventEmitter<any>();
-  constructor(private dragulaService: DragulaService) {}
+export class MealtimeComponent {
+    @Input() public title: string;
+    @Input() public model: string;
+    @Input() public mealtime: Mealtimes;
+    @Input() public errorTexts: Errortext[];
+    @Output() public event: EventEmitter<Recipe> = new EventEmitter<Recipe>();
+    @Output() public errorEvent: EventEmitter<any> = new EventEmitter<any>();
+    private templateParser: TemplateParser = new TemplateParser();
+    constructor(private dragulaService: DragulaService) {}
 
-  ngOnInit() {}
+    public addRecipe(value: Recipe) {
+        if (this.matchMealtimes(value.mealtime)) {
+            this.event.emit(value);
+        } else {
+            const mealTimeErrorText: Errortext = new Errortext().deserialize(
+                this.errorTexts.find(
+                    (errorText: Errortext) => errorText.name === "mealtime"
+                )
+            );
+            this.dragulaService.find("recipes").drake.cancel(true);
 
-  addRecipe(value: Recipe) {
-    if (this.matchMealtimes(value.mealtime)) {
-      this.event.emit(value);
-    } else {
-      this.dragulaService.find("recipes").drake.cancel(true);
-      this.errorEvent.emit(
-        this.errorTexts.find(errorText => errorText.name === "mealtime")
-      );
+            mealTimeErrorText.text = this.templateParser.parse(
+                mealTimeErrorText.text,
+                [value.name, this.mealtime]
+            );
+            this.errorEvent.emit(mealTimeErrorText);
+        }
     }
-  }
 
-  matchMealtimes(mealtime: Mealtimes): boolean {
-    return this.mealtime === mealtime;
-  }
+    public matchMealtimes(mealtime: Mealtimes): boolean {
+        return this.mealtime === mealtime;
+    }
 }
--- src/app/components/game/shopping-list/shopping-list.component.ts
+++ src/app/components/game/shopping-list/shopping-list.component.ts
@@ -1,133 +1,147 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { DragulaService } from 'ng2-dragula';
-import { Subject, Subscription } from 'rxjs';
-import { IngredientService } from 'src/app/providers/ingredient.service';
-import { FridgeStoreService } from 'src/app/providers/store/fridge-store.service';
-import { RecipeStoreService } from 'src/app/providers/store/recipe-store.service';
-import { ShoppingListStoreService } from 'src/app/providers/store/shopping-list-store.service';
-import { Errortext } from 'src/lib/models/Errortext';
-import { Game } from 'src/lib/models/Game';
-import { Ingredient } from 'src/lib/models/Ingredient';
-import { Recipe } from 'src/lib/models/Recipe';
-import { Word } from 'src/lib/models/Word';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { DragulaService } from "ng2-dragula";
+import { Subject, Subscription } from "rxjs";
+import { IngredientService } from "src/app/providers/ingredient.service";
+import { FridgeStoreService } from "src/app/providers/store/fridge-store.service";
+import { RecipeStoreService } from "src/app/providers/store/recipe-store.service";
+import { ShoppingListStoreService } from "src/app/providers/store/shopping-list-store.service";
+import { Errortext } from "src/lib/models/Errortext";
+import { Game } from "src/lib/models/Game";
+import { Ingredient } from "src/lib/models/Ingredient";
+import { Recipe } from "src/lib/models/Recipe";
+import { Word } from "src/lib/models/Word";
+import { TemplateParser } from "src/lib/utils/TemplateParser";
 
-import { GameComponent } from '../game.component';
+import { GameComponent } from "../game.component";
 
 @Component({
-  selector: "serious-game-shopping-list",
-  templateUrl: "./shopping-list.component.html",
-  styleUrls: ["./shopping-list.component.scss"]
+    selector: "serious-game-shopping-list",
+    templateUrl: "./shopping-list.component.html",
+    styleUrls: ["./shopping-list.component.scss"]
 })
 export class ShoppingListComponent implements OnInit, GameComponent {
-  @Input() game: Game;
-  @Input() data: (Recipe | Word)[];
-  @Input() errorTexts: Errortext[];
-  @Input() mainGameSubject: Subject<any>;
-  @Output() event: EventEmitter<any> = new EventEmitter();
-  @Output() errorEvent: EventEmitter<any> = new EventEmitter();
-  private name = "shoppinglist";
-  private ingredients: Ingredient[];
-  private subscription: Subscription = new Subscription();
-  private shoppingListItems: Ingredient[];
-  private fridgeItems: Ingredient[];
-  private itemsValid = false;
+    @Input() public game: Game;
+    @Input() public data: Array<Recipe | Word>;
+    @Input() public errorTexts: Errortext[];
+    @Input() public mainGameSubject: Subject<any>;
+    @Output() public event: EventEmitter<any> = new EventEmitter();
+    @Output() public errorEvent: EventEmitter<any> = new EventEmitter();
+    private name = "shoppinglist";
+    private ingredients: Ingredient[];
+    private subscription: Subscription = new Subscription();
+    private shoppingListItems: Ingredient[];
+    private fridgeItems: Ingredient[];
+    private itemsValid = false;
+    private templateParser: TemplateParser = new TemplateParser();
 
-  constructor(
-    private ingredientService: IngredientService,
-    private shoppingListStore: ShoppingListStoreService,
-    private dragulaService: DragulaService,
-    private fridgeStore: FridgeStoreService,
-    private recipeStore: RecipeStoreService
-  ) {}
+    constructor(
+        private ingredientService: IngredientService,
+        private shoppingListStore: ShoppingListStoreService,
+        private dragulaService: DragulaService,
+        private fridgeStore: FridgeStoreService,
+        private recipeStore: RecipeStoreService
+    ) {}
 
-  ngOnInit() {
-    this.subscription.add(
-      this.ingredientService.getAll().subscribe(ingredient => {
-        this.ingredients = ingredient;
-      })
-    );
-    this.subscription.add(
-      this.dragulaService.dropModel(this.name).subscribe(value => {
-        const item = this.data.find(element => element.id === +value.el.id);
-        if (value.target.id === "drag") {
-          this.removeItem(item);
+    public ngOnInit() {
+        this.subscription.add(
+            this.ingredientService.getAll().subscribe((ingredient) => {
+                this.ingredients = ingredient;
+            })
+        );
+        this.subscription.add(
+            this.dragulaService.dropModel(this.name).subscribe((value) => {
+                const item = this.data.find(
+                    (element) => element.id === +value.el.id
+                );
+                if (value.target.id === "drag") {
+                    this.removeItem(item);
+                } else {
+                    this.addItem(item);
+                    this.dragulaService.find(this.name).drake.cancel(true);
+                }
+            })
+        );
+        this.subscription.add(
+            this.shoppingListStore.items$.subscribe((items) => {
+                this.shoppingListItems = items;
+            })
+        );
+        this.subscription.add(
+            this.fridgeStore.items$.subscribe((items) => {
+                this.fridgeItems = items;
+            })
+        );
+    }
+    /**
+     * adds item to shopping list
+     * @param item Ingredient|Word
+     */
+    public addItem(item) {
+        const shoppingListErrorText: Errortext = new Errortext();
+        if (!this.fridgeStore.alreadyRandomized) {
+            shoppingListErrorText.deserialize(
+                this.errorTexts.find(
+                    (errorText) => errorText.name === "fridge-not-checked"
+                )
+            );
+            this.errorEvent.emit(shoppingListErrorText);
+        } else if (!this.validShoppingListItem(item)) {
+            shoppingListErrorText.deserialize(
+                this.errorTexts.find(
+                    (errorText) => errorText.name === "item-already-in-fridge"
+                )
+            );
+            shoppingListErrorText.text = this.templateParser.parse(
+                shoppingListErrorText.text,
+                [item.name]
+            );
+            this.errorEvent.emit(shoppingListErrorText);
         } else {
-          this.addItem(item);
-          this.dragulaService.find(this.name).drake.cancel(true);
+            this.shoppingListStore.addItem(item);
+            if (this.compareShoppingListWithRecipe()) {
+                this.event.emit();
+            }
         }
-      })
-    );
-    this.subscription.add(
-      this.shoppingListStore.items$.subscribe(items => {
-        this.shoppingListItems = items;
-      })
-    );
-    this.subscription.add(
-      this.fridgeStore.items$.subscribe(items => {
-        this.fridgeItems = items;
-      })
-    );
-  }
-  /**
-   * adds item to shopping list
-   * @param item Ingredient|Word
-   */
-  addItem(item) {
-    if (!this.fridgeStore.alreadyRandomized) {
-      this.errorEvent.emit(
-        this.errorTexts.find(
-          errorText => errorText.name === "fridge-not-checked"
-        )
-      );
-    } else if (!this.validShoppingListItem(item)) {
-      this.errorEvent.emit(
-        this.errorTexts.find(
-          errorText => errorText.name === "item-already-in-fridge"
-        )
-      );
-    } else {
-      this.shoppingListStore.addItem(item);
-      if (this.compareShoppingListWithRecipe()) {
-        this.event.emit();
-      }
     }
-  }
 
-  /**
-   * compares the ingredients defined in the recipe with the items
-   * of the fridge + the items in the shopping list
-   * @return whether the shopping list contains every needed item or not
-   */
-  compareShoppingListWithRecipe(): boolean {
-    this.recipeStore.currentRecipe.ingredients.map(ingredient => {
-      this.itemsValid =
-        this.shoppingListItems
-          .concat(this.fridgeItems)
-          .findIndex(
-            recipeIngredient => recipeIngredient.id === ingredient.id
-          ) > -1;
-    });
-    return this.itemsValid;
-  }
-
-  /**
-   * removes item from the shopping list
-   * @param item Ingredient|Word
-   */
-  removeItem(item) {
-    this.shoppingListStore.removeItem(item);
-    if (this.compareShoppingListWithRecipe()) {
-      this.event.emit();
+    /**
+     * compares the ingredients defined in the recipe with the items
+     * of the fridge + the items in the shopping list
+     * @return whether the shopping list contains every needed item or not
+     */
+    public compareShoppingListWithRecipe(): boolean {
+        this.recipeStore.currentRecipe.ingredients.forEach((ingredient) => {
+            this.itemsValid =
+                this.shoppingListItems
+                    .concat(this.fridgeItems)
+                    .findIndex(
+                        (recipeIngredient) =>
+                            recipeIngredient.id === ingredient.id
+                    ) > -1;
+        });
+        return this.itemsValid;
     }
-  }
 
-  /**
-   * checks if item added to the shopping list is valid
-   * @param item Ingredient|Word
-   */
-  validShoppingListItem(item: Ingredient | Word): boolean {
-    return (
-      this.fridgeItems.findIndex(fridgeItem => fridgeItem.id === item.id) === -1
-    );
-  }
+    /**
+     * removes item from the shopping list
+     * @param item Ingredient|Word
+     */
+    public removeItem(item) {
+        this.shoppingListStore.removeItem(item);
+        if (this.compareShoppingListWithRecipe()) {
+            this.event.emit();
+        }
+    }
+
+    /**
+     * checks if item added to the shopping list is valid
+     * @param item Ingredient|Word
+     */
+    public validShoppingListItem(item: Ingredient | Word): boolean {
+        return (
+            this.fridgeItems.findIndex(
+                (fridgeItem) => fridgeItem.id === item.id
+            ) === -1
+        );
+    }
 }
