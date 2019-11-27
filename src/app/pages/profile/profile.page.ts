672d3ab7935597855812650884aa7417142b62e3
Florian Mold <florian.mold@live.at>
2019-11-27 20:21:16 +0100

    [FEATURE] profile update feature

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,31 +1,33 @@
 import { Component, OnInit } from "@angular/core";
-import { User } from "src/lib/models/User";
+import { Subscription } from "rxjs";
 import { AuthService } from "src/app/providers/auth.service";
 import { UserStoreService } from "src/app/providers/store/user-store.service";
-import { Subscription } from "rxjs";
+import { User } from "src/lib/models/User";
 @Component({
-  selector: "serious-game-profile",
-  templateUrl: "./profile.page.html",
-  styleUrls: ["./profile.page.scss"]
+    selector: "serious-game-profile",
+    templateUrl: "./profile.page.html",
+    styleUrls: ["./profile.page.scss"]
 })
 export class ProfilePage {
-  private user: User;
-  private isTherapist: boolean;
-  private subscription: Subscription = new Subscription();
-  constructor(
-    private authService: AuthService,
-    private userStore: UserStoreService
-  ) {}
+    private user: User;
+    private isTherapist: boolean;
+    private subscription: Subscription = new Subscription();
 
-  ionViewWillEnter() {
-    this.isTherapist = this.authService.isTherapist();
-    this.subscription.add(
-      this.userStore.user.subscribe(user => {
-        this.user = user;
-      })
-    );
-  }
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
+    constructor(
+        private authService: AuthService,
+        private userStore: UserStoreService
+    ) {}
+
+    public ionViewWillEnter() {
+        this.isTherapist = this.authService.isTherapist();
+        this.subscription.add(
+            this.userStore.user.subscribe((user) => {
+                this.user = user;
+            })
+        );
+    }
+
+    public ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 }
