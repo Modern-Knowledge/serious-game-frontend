4b6ff371f9032c234a00a438d03a579f596bde0d
Florian Mold <florian.mold@live.at>
2019-11-29 09:38:29 +0100

    [FEATURE] profile-edit: add form to profile

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
 
 import { IonicModule } from "@ionic/angular";
@@ -28,7 +28,8 @@ const routes: Routes = [
         CommonModule,
         FormsModule,
         IonicModule,
-        RouterModule.forChild(routes)
+        RouterModule.forChild(routes),
+        ReactiveFormsModule
     ],
 })
 export class ProfilePageModule {}
--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,22 +1,57 @@
-import { Component, OnInit } from "@angular/core";
-import { Subscription } from "rxjs";
-import { AuthService } from "src/app/providers/auth.service";
-import { UserStoreService } from "src/app/providers/store/user-store.service";
-import { User } from "src/lib/models/User";
+import {Component, OnInit} from "@angular/core";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {AuthService} from "src/app/providers/auth.service";
+import {UserStoreService} from "src/app/providers/store/user-store.service";
+import {User} from "src/lib/models/User";
+import {UserService} from "../../providers/user.service";
+
 @Component({
     selector: "serious-game-profile",
-    templateUrl: "./profile.page.html",
-    styleUrls: ["./profile.page.scss"]
+    styleUrls: ["./profile.page.scss"],
+    templateUrl: "./profile.page.html"
 })
-export class ProfilePage {
+export class ProfilePage implements OnInit {
     private user: User;
     private isTherapist: boolean;
     private subscription: Subscription = new Subscription();
+    private changeProfileForm: FormGroup;
 
+    /**
+     * @param authService authentication service
+     * @param userStore user store
+     * @param userService user service
+     */
     constructor(
         private authService: AuthService,
-        private userStore: UserStoreService
+        private userStore: UserStoreService,
+        private userService: UserService
     ) {}
+
+    /**
+     * Creates the change profile form.
+     */
+    public ngOnInit() {
+        this.changeProfileForm = new FormGroup({
+            email: new FormControl("", [Validators.email, Validators.required]),
+            forename: new FormControl("", Validators.required),
+            lastname: new FormControl("", Validators.required),
+        });
+    }
+
+    public onSubmit() {
+        this.subscription.add(
+            this.userService
+                .updateUser(
+                    this.user.id,
+                    this.changeProfileForm.controls.email.value,
+                    this.changeProfileForm.controls.forename.value,
+                    this.changeProfileForm.controls.lastname.value
+                ).subscribe((response) => {
+                    console.log(response);
+            })
+        );
+    }
 
     public ionViewWillEnter() {
         this.isTherapist = this.authService.isTherapist();
