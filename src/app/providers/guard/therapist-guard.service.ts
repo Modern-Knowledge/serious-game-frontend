039c72b56086ac47216bf8b083eca597830d4382
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-12-07 11:33:21 +0100

    [FEATURE] add guard to check if therapist is patient

---
+++ src/app/providers/guard/therapist-guard.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router } from "@angular/router";
+import { ToastPosition, ToastWrapper } from "src/app/util/ToastWrapper";
+import { HttpResponseMessageSeverity } from "src/lib/utils/http/HttpResponse";
+
+import { AuthService } from "../auth.service";
+
+@Injectable({
+    providedIn: "root"
+})
+export class TherapistGuardService implements CanActivate {
+    constructor(private authService: AuthService, private router: Router) {}
+
+    public canActivate(route: ActivatedRouteSnapshot): boolean {
+        if (!this.authService.isTherapist()) {
+            const message = new ToastWrapper(
+                "Diese Funktion ist nur für TherapeutInnen vorgesehen.",
+                ToastPosition.TOP,
+                HttpResponseMessageSeverity.DANGER
+            );
+            message.alert();
+            this.router.navigateByUrl("main-menu");
+            return false;
+        }
+        return true;
+    }
+}
