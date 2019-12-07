3db5d9f71561f69f7d5b9be363c31c8d9d346ab5
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-12-07 13:34:52 +0100

    [BUGFIX] display images in shopping cart

--- src/app/components/shared/cart/cart.component.ts
+++ src/app/components/shared/cart/cart.component.ts
@@ -6,51 +6,53 @@ import { Ingredient } from "src/lib/models/Ingredient";
 import { Word } from "src/lib/models/Word";
 
 @Component({
-  selector: "serious-game-cart",
-  templateUrl: "./cart.component.html",
-  styleUrls: ["./cart.component.scss"]
+    selector: "serious-game-cart",
+    styleUrls: ["./cart.component.scss"],
+    templateUrl: "./cart.component.html"
 })
 export class CartComponent implements OnInit {
-  @Input() public id: string;
-  @Input() public data: Array<Ingredient | Word>;
-  @Input() public name: string;
-  @Input() public viewing: boolean;
-  private items: Array<Ingredient | Word>;
-  private subscriptions: Subscription = new Subscription();
+    @Input() public id: string;
+    @Input() public data: Array<Ingredient | Word>;
+    @Input() public name: string;
+    @Input() public viewing: boolean;
+    public items: Array<Ingredient | Word>;
+    private subscriptions: Subscription = new Subscription();
 
-  constructor(
-    private cartStore: CartStoreService,
-    private dragulaService: DragulaService
-  ) {}
+    constructor(
+        private cartStore: CartStoreService,
+        private dragulaService: DragulaService
+    ) {}
 
-  public ngOnInit() {
-    this.subscriptions.add(
-      this.cartStore.items$.subscribe((items) => {
-        this.items = items;
-      })
-    );
-    this.subscriptions.add(
-      this.dragulaService.dropModel(this.name).subscribe((value) => {
-        const item = this.data.find((element) => element.id === +value.el.id);
-        if (value.target.id === "shelf") {
-          this.removeFoodItem(item);
-        } else {
-          this.addFoodItem(item);
-          this.dragulaService.find(this.name).drake.cancel(true);
-        }
-      })
-    );
-  }
+    public ngOnInit() {
+        this.subscriptions.add(
+            this.cartStore.items$.subscribe((items) => {
+                this.items = items;
+            })
+        );
+        this.subscriptions.add(
+            this.dragulaService.dropModel(this.name).subscribe((value) => {
+                const item = this.data.find(
+                    (element) => element.id === +value.el.id
+                );
+                if (value.target.id === "shelf") {
+                    this.removeFoodItem(item);
+                } else {
+                    this.addFoodItem(item);
+                    this.dragulaService.find(this.name).drake.cancel(true);
+                }
+            })
+        );
+    }
 
-  public addFoodItem(item) {
-    this.cartStore.addItem(item);
-  }
+    public addFoodItem(item) {
+        this.cartStore.addItem(item);
+    }
 
-  public removeFoodItem(item) {
-    this.cartStore.removeItem(item);
-  }
+    public removeFoodItem(item) {
+        this.cartStore.removeItem(item);
+    }
 
-  public ngOnDestroy() {
-    this.subscriptions.unsubscribe();
-  }
+    public ngOnDestroy() {
+        this.subscriptions.unsubscribe();
+    }
 }
