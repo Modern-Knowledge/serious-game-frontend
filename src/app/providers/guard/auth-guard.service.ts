c335cebea3eb4b4f787b8b5f6b7e01b38c8b0552
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-12-07 11:32:53 +0100

    [FEATURE] add auth guard to check if user is logged in

---
+++ src/app/providers/guard/auth-guard.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router } from "@angular/router";
+import { ToastPosition, ToastWrapper } from "src/app/util/ToastWrapper";
+import { HttpResponseMessageSeverity } from "src/lib/utils/http/HttpResponse";
+
+import { AuthService } from "../auth.service";
+
+@Injectable({
+    providedIn: "root"
+})
+export class AuthGuardService implements CanActivate {
+    constructor(private authService: AuthService, private router: Router) {}
+
+    public canActivate(route: ActivatedRouteSnapshot): boolean {
+        if (!this.authService.isLoggedIn()) {
+            const message = new ToastWrapper(
+                "Sie sind nicht eingeloggt.",
+                ToastPosition.TOP,
+                HttpResponseMessageSeverity.DANGER
+            );
+            message.alert();
+            this.router.navigateByUrl("login");
+            return false;
+        }
+        return true;
+    }
+}
