e558abca0c997b5dc9af1340b9585cf5df0d8469
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-12-07 11:34:01 +0100

    [BUGFIX] hide game button for therapists

--- src/app/pages/main-menu/main-menu.page.ts
+++ src/app/pages/main-menu/main-menu.page.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/providers/auth.service";
 
 @Component({
-  selector: "serious-game-main-menu",
-  styleUrls: ["./main-menu.page.scss"],
-  templateUrl: "./main-menu.page.html"
+    selector: "serious-game-main-menu",
+    styleUrls: ["./main-menu.page.scss"],
+    templateUrl: "./main-menu.page.html"
 })
 export class MainMenuPage implements OnInit {
-  constructor() {
+    public isTherapist: boolean;
 
-  }
+    constructor(private authService: AuthService) {}
 
-  public ngOnInit() {}
+    public ngOnInit() {}
+    public ionViewWillEnter() {
+        this.isTherapist = this.authService.isTherapist();
+    }
 }
