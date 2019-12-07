74f9daa4dc799de8fc596ff8b59a64465278d444
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-12-07 11:34:12 +0100

    [FEATURE] use guards to protect routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,49 +1,67 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
+import { AuthGuardService } from "./providers/guard/auth-guard.service";
+import { PatientGuardService } from "./providers/guard/patient-guard.service";
+
 const routes: Routes = [
     { path: "", redirectTo: "login", pathMatch: "full" },
     { path: "home", loadChildren: "./pages/home/home.module#HomePageModule" },
-    { path: "login", loadChildren: "./pages/login/login.module#LoginPageModule" },
+    {
+        path: "login",
+        loadChildren: "./pages/login/login.module#LoginPageModule"
+    },
     {
         path: "password-reset",
-        loadChildren: "./pages/password-reset/password-reset.module#PasswordResetModule"
+        loadChildren:
+            "./pages/password-reset/password-reset.module#PasswordResetModule"
     },
     {
         path: "reset-password",
-        loadChildren: "./pages/reset-password/reset-password.module#ResetPasswordModule"
+        loadChildren:
+            "./pages/reset-password/reset-password.module#ResetPasswordModule"
     },
     {
         path: "registration",
-        loadChildren: "./pages/registration/registration.module#RegistrationPageModule"
+        loadChildren:
+            "./pages/registration/registration.module#RegistrationPageModule"
     },
     {
         path: "profile",
-        loadChildren: "./pages/profile/profile.module#ProfilePageModule"
+        loadChildren: "./pages/profile/profile.module#ProfilePageModule",
+        canActivate: [AuthGuardService]
     },
     {
         path: "main-menu",
-        loadChildren: "./pages/main-menu/main-menu.module#MainMenuPageModule"
+        loadChildren: "./pages/main-menu/main-menu.module#MainMenuPageModule",
+        canActivate: [AuthGuardService]
     },
     {
         path: "game",
-        loadChildren: "./pages/game/game.module#GamePageModule"
+        loadChildren: "./pages/game/game.module#GamePageModule",
+        canActivate: [AuthGuardService, PatientGuardService]
     },
     {
         path: "fridge",
-        loadChildren: "./pages/game/fridge/fridge.module#FridgePageModule"
+        loadChildren: "./pages/game/fridge/fridge.module#FridgePageModule",
+        canActivate: [AuthGuardService, PatientGuardService]
     },
     {
         path: "shelf/:id",
-        loadChildren: "./pages/game/shelf/shelf.module#ShelfPageModule"
+        loadChildren: "./pages/game/shelf/shelf.module#ShelfPageModule",
+        canActivate: [AuthGuardService, PatientGuardService]
     },
     {
         path: "score-board",
-        loadChildren: "./pages/game/score-board/score-board.module#ScoreBoardPageModule"
+        loadChildren:
+            "./pages/game/score-board/score-board.module#ScoreBoardPageModule",
+        canActivate: [AuthGuardService]
     },
     {
         path: "change-password",
-        loadChildren: "./pages/user/change-password/change-password.module#ChangePasswordPageModule"
+        loadChildren:
+            "./pages/user/change-password/change-password.module#ChangePasswordPageModule",
+        canActivate: [AuthGuardService]
     }
 ];
 
