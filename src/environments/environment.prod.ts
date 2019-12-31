554c3f21397f42ad795928f2757ba75139a19416
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2019-12-31 14:23:18 +0100

    [TASK] set up prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+    backendUrl: "http://localhost:3000",
+    logging: {
+        ajaxAppender: {
+            threshold: "ERROR",
+            url: "http://localhost:3000/logging/"
+        },
+        logLevels: [
+            {
+                logLevel: "ERROR",
+                loggerName: "root"
+            }
+        ]
+    },
+    production: true,
+    passwordLength: 6,
+    tokenLength: 8
 };
