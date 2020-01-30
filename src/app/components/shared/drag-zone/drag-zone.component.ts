91ac83c1f6f4dbbb71c28e8d8a9bdf81989d82b4
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2020-01-30 21:08:12 +0100

    [BUGFIX] disable scrolling when dragging

--- src/app/components/shared/drag-zone/drag-zone.component.ts
+++ src/app/components/shared/drag-zone/drag-zone.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from "@angular/core";
+import { DragulaService } from 'ng2-dragula';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: "serious-game-drag-zone",
@@ -9,4 +11,28 @@ export class DragZoneComponent {
     @Input() public id: string;
     @Input() public name: string;
     @Input() public model: string;
+
+    private subscription: Subscription = new Subscription();
+
+    constructor(private dragulaService: DragulaService) {}
+
+    public ngOnInit() {
+        const options: AddEventListenerOptions = {
+            passive: false
+        }
+
+        const listener = e => e.preventDefault();
+
+        this.subscription.add(
+            this.dragulaService.drag(this.name).subscribe((value) => {
+                document.addEventListener("touchmove", listener, options);
+            })
+        );
+        this.subscription.add(
+            this.dragulaService.drop(this.name).subscribe((value) => {
+                
+                document.removeEventListener("touchmove",  listener, options);
+            })
+        );
+    }
 }
