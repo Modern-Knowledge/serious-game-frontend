ecade06d56d8afd8e99ae3275244eb8e51ce222c
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2020-02-03 20:33:49 +0100

    [BUGFIX] fix autoscrolling when nearing edge of screen
    using dom-autoscroller

--- src/app/components/shared/drag-zone/drag-zone.component.ts
+++ src/app/components/shared/drag-zone/drag-zone.component.ts
@@ -1,15 +1,15 @@
-import { Component, Input } from "@angular/core";
-import { DragulaService } from 'ng2-dragula';
-import { Subscription } from 'rxjs';
-import autoScroll from "dom-autoscroller"
-import { IonContent } from '@ionic/angular';
+import { Component, Input, OnInit } from "@angular/core";
+import { IonContent } from "@ionic/angular";
+import autoScroll from "dom-autoscroller";
+import { DragulaService } from "ng2-dragula";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: "serious-game-drag-zone",
     styleUrls: ["./drag-zone.component.scss"],
     templateUrl: "./drag-zone.component.html"
 })
-export class DragZoneComponent {
+export class DragZoneComponent implements OnInit {
     @Input() public id: string;
     @Input() public name: string;
     @Input() public model: string;
@@ -19,45 +19,33 @@ export class DragZoneComponent {
 
     constructor(private dragulaService: DragulaService) {}
 
-    getOffset( el ) {
-        var _x = 0;
-        var _y = 0;
-        while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-            _x += el.offsetLeft - el.scrollLeft;
-            _y += el.offsetTop - el.scrollTop;
-            el = el.offsetParent;
-        }
-        return { top: _y, left: _x };
-    }
-
     public ngOnInit() {
         const options: AddEventListenerOptions = {
             passive: false
-        }
-        let offset = 0;
-        const listener = e => {
+        };
+        const listener = (e) => {
             e.preventDefault();
-            const y = this.getOffset(e.srcElement).top;
-            
-            Promise.resolve(this.scrollElement.getScrollElement()).then(element => {
-                if(y >= element.clientHeight-100){
-                    this.scrollElement.scrollToBottom()
-                    offset = y;
-                }
-                else if(y+offset <= element.clientHeight+100){
-                    this.scrollElement.scrollToTop();
-                }
-            })
         };
         this.subscription.add(
             this.dragulaService.drag(this.name).subscribe((value) => {
                 document.addEventListener("touchmove", listener, options);
+                Promise.resolve(this.scrollElement.getScrollElement()).then(
+                    (element) => {
+                        autoScroll([element], {
+                            direction: "vertical",
+                            margin: 50,
+                            maxSpeed: 10,
+                            autoScroll() {
+                                return true;
+                            }
+                        });
+                    }
+                );
             })
         );
         this.subscription.add(
             this.dragulaService.drop(this.name).subscribe((value) => {
-                
-                document.removeEventListener("touchmove",  listener, options);
+                document.removeEventListener("touchmove", listener, options);
             })
         );
     }
