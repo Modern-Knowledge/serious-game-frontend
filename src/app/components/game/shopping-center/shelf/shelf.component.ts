05f00265cfa072869cfd148e4756fba018005963
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2020-02-03 20:33:13 +0100

    [FEATURE] pass scrollContainer element to game components

--- src/app/components/game/shopping-center/shelf/shelf.component.ts
+++ src/app/components/game/shopping-center/shelf/shelf.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { IonContent } from "@ionic/angular";
 import { Subscription } from "rxjs";
 import { IngredientService } from "src/app/providers/ingredient.service";
 import { FoodCategory } from "src/lib/models/FoodCategory";
@@ -11,6 +12,7 @@ import { Ingredient } from "src/lib/models/Ingredient";
 })
 export class ShelfComponent implements OnInit, OnDestroy {
     @Input() public shelf: FoodCategory;
+    @Input() public scrollContainer: IonContent;
     public foodItems: Ingredient[];
     private subscription: Subscription = new Subscription();
 
