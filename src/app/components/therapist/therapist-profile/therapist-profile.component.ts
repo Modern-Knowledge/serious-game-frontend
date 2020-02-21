d88aad6107e93df8fa4765515bbe6a19871c4d66
Daniel Kaufmann <e1639360@student.tuwien.ac.at>
2020-02-21 16:25:32 +0100

    [BUGFIX] adjust therapist patient updating to dtos

--- src/app/components/therapist/therapist-profile/therapist-profile.component.ts
+++ src/app/components/therapist/therapist-profile/therapist-profile.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnDestroy } from "@angular/core";
 import { Subscription } from "rxjs";
 import { TherapistService } from "src/app/providers/therapist.service";
 import { TherapistDto } from "src/lib/models/Dto/TherapistDto";
-import { Therapist } from "src/lib/models/Therapist";
 
 @Component({
     selector: "serious-game-therapist-profile",
@@ -10,18 +9,14 @@ import { Therapist } from "src/lib/models/Therapist";
     templateUrl: "./therapist-profile.component.html"
 })
 export class TherapistProfileComponent implements OnDestroy {
-    @Input() public user: Therapist;
+    @Input() public user: TherapistDto;
     private subscription: Subscription = new Subscription();
 
     constructor(private therapistService: TherapistService) {}
 
     public assignPatients(patients) {
         this.user.patients = patients;
-        this.subscription.add(
-            this.therapistService
-                .update(new TherapistDto(this.user))
-                .subscribe()
-        );
+        this.therapistService.update(this.user).subscribe();
     }
 
     public ngOnDestroy() {
